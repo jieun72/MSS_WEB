004672eadd1110f0d46b6a89954f7b8bc4a414c9
jieun72 <lee.jieun@kesti.co.kr>
2023-03-09 10:29:26 +0900

    230309 카카오api 연결테스트

--- src/main/java/com/mss/controller/MonitoringController.java
+++ src/main/java/com/mss/controller/MonitoringController.java
@@ -1,5 +1,6 @@
 package com.mss.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +10,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/monitoring")
 public class MonitoringController {
 
+    @Value("${kakao.apikey}")
+    private String apiKey;
+
     /* 화면 초기화 */
     @RequestMapping(method = RequestMethod.GET)
     public String init(final Model model) throws Exception {
 
+        String mapKey = "http://dapi.kakao.com/v2/maps/sdk.js?appkey=" + apiKey + "&libraries=services,clusterer,drawing";
+
         // TODO: 현재시각 설정
         model.addAttribute("nowTime", "2023-03-07 13:00:00");
+        model.addAttribute("mapKey", mapKey);
 
         return "Monitoring";
     }
--- src/main/resources/static/js/Monitoring.js
+++ src/main/resources/static/js/Monitoring.js
@@ -17,7 +17,25 @@ $(document).ready(function(){
         $("#infoArea").addClass("infoAreaMobile");
     }
 
-    // TODO: kakao map 표시
+    // // kakao map 생성
+    // var container = document.getElementById('mapArea');
+    // var options = {
+    //     center: new kakao.maps.LatLng(37.56, 127), // 서울시
+    //     level: 8
+    // };
+    //
+    // this.map = new kakao.maps.Map(container, options);
+    // this.map.addOverlayMapTypeId(kakao.maps.MapTypeId.TERRAIN);
+    //
+    // var zoomControl = new kakao.maps.ZoomControl();
+    // this.map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+    // leaflet을 이용한 지도 생성
+    var mymap = L.map('mapArea').setView([37.56, 127], 12);
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        maxZoom: 15
+    }).addTo(mymap);
+
     // TODO: 검색 로직1-지도마커
 
     // TODO: 검색 로직2-선택 지점
